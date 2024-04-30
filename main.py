5ec700eaba6073b98be25143bebb71f7c1aeaebf
taoista <taoista-@hotmail.com>
2024-04-30 18:25:05 -0400

    demo

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-
+import os
 
 
 
 
 
 def main():
+    path_file = get_path_folder()
     print("started")
+    print(path_file)
+
+
+def get_path_folder():
+    return os.path.dirname(os.path.realpath(__file__))
 
 
 if __name__ == "__main__":
